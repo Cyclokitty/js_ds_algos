ca20ab1755c42eb1ec43b8ffd6060fa941a358cc
Cyclokitty <laura@vingada.com>
2019-08-07 18:42:40 -0400

    completed palindrome exercise

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,39 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+
+// recursive
+// helper functions:
+function firstLetter(str) {
+    return str.slice(0, 1);
+}
+
+function lastLetter(str) {
+    return str.slice(-1);
+}
+
+function middleWord(str) {
+    return str.slice(1, -1);
+}
+
+function palindrome(str) {
+    if (str.length < 2) return true;
+
+    if (firstLetter(str) === lastLetter(str)) return palindrome(middleWord(str));
+
+    return false;
+}
+
+// using the .reduce method from reversestring
+// function palindrome(str) {
+//     // reverse word
+//     let revWord = str.split('').reduce((rev, char) => char + rev, '');
+//     return str === revWord ? true : false;
+
+//     // if reversed word equals str return true, else return false
+// }
+
+palindrome('madam');
+palindrome('wex');
 
 module.exports = palindrome;

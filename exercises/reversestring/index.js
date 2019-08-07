d0049c32b4cea25e5708a78da7c03619297f63f5
Cyclokitty <laura@vingada.com>
2019-08-07 18:05:28 -0400

    added algos to reverse string lesson

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// using the .reduce()
+function reverse(str) {                                                                            
+    return str.split('').reduce((reversed, character) => character + reversed
+    , '');
+}
+
+// function reverse(str) {
+//     let newStr = '';
+//     for ( let character of str) {
+//         newStr = character + newStr;
+//     }
+//     return newStr;
+// }
+
+// this one is mine:
+// function reverse(str) {
+//     let newStr = '';
+//     for ( let i = str.length - 1; i >= 0; i-- ) {
+//         newStr += str[i];
+//     }
+//     return newStr;
+// }
+
+// using the built in functions
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+console.log(reverse('liver'));
+console.log(reverse('muhaha'));
 
 module.exports = reverse;

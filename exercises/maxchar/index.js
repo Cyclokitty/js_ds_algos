5c6a28c60544fb3346cb04340af8cc0f40b752ed
Cyclokitty <laura@vingada.com>
2019-08-08 16:49:15 -0400

    completed maxchar exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let chars = {};
+
+    str = str.split('');
+
+    for (let char of str) {
+        typeof chars[char] === 'undefined' ? chars[char] = 1 : chars[char]++;
+    }
+
+    let max = 0;
+    let maxChar = '';
+    for (let key in chars) {
+        if (chars[key] >= max) {
+            max = chars[key];
+            maxChar = key;
+        }
+    }
+    return maxChar;
+
+}
 
 module.exports = maxChar;

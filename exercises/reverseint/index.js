7bf5230b2067956a44e262a8cacf110878218fae
Cyclokitty <laura@vingada.com>
2019-08-08 12:28:39 -0400

    completed reverseint exercise

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// helper functions
+function numberToString(number) {
+    return number.toString();
+    }
+
+    function reverseString(str) {
+        return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+function reverseInt(n) {
+    if (n === 0) return 0;
+
+    if (n > 0) {
+        return Number(reverseString(numberToString(n)));
+    }
+    
+    if (n < 0) {
+        let newNum = Math.abs(n);
+        newNum = Number(reverseString(numberToString(newNum)));
+        return -Math.abs(newNum);
+    }
+}
 
 module.exports = reverseInt;

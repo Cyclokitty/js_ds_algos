1fc07fd2dbd55fc0ee705e8d45e9fff2d4375a11
Cyclokitty <laura@vingada.com>
2019-08-13 13:01:10 -0400

    added another solution to steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,20 +17,43 @@
 //       '### '
 //       '####'
 
-// my solution
-function steps(n) {
-    let stepper = 1;
 
-    if (n < 0) return '';
+// my solution
+// function steps(n) {
+//     let stepper = 1;
+
+//     if (n < 0) return '';
     
-    while (stepper <= n) {    
-        if (stepper === n) {
-            console.log('#'.repeat(stepper));
-        } else {
-            console.log('#'.repeat(stepper) + ' '.repeat(n - stepper));
+//     while (stepper <= n) {    
+//         if (stepper === n) {
+//             console.log('#'.repeat(stepper));
+//         } else {
+//             console.log('#'.repeat(stepper) + ' '.repeat(n - stepper));
+//         }
+//         stepper++;
+//     }  
+// }
+
+function steps(n) {
+    // from 0 to n(iterate thru rows)
+    for (let i = 0; i < n; i++) {
+      // create an empty string, 'stair'
+        let stair = '';
+      // from 0 to n (iterate thru columns)
+        for (let j = 0; j < n; j++) {
+        // if the current column is equal to or lessthan the current row:
+        if (j <= i) {
+          // add a '#' to the stair
+            stair += '#';
+        } // else 
+            else {
+            // ad a space to stair
+            stair += ' ';
         }
-        stepper++;
-    }  
+    }
+      // console.log stair
+        console.log(stair);  
+    }    
 }
 
 module.exports = steps;

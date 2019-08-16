622f227cf8d0b0b880a4d1f261a479a481698147
Cyclokitty <laura@vingada.com>
2019-08-16 10:52:49 -0400

    completed vowels algo

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,36 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// more condensed solution
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
+
+// iterative
+// function vowels(str) {
+//     let counter = 0;
+//     const checker = ['a', 'e', 'i', 'o', 'u'];
+
+//     for (let char of str.toLowerCase()) {
+//         if (checker.includes(char)) {
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+
+// my solution
+// function vowels(str) {
+//     const regexVowels = /[aeiou]/gi;
+//     let counter = 0;
+//     for (let letter of str) {
+//         if (letter.match(regexVowels)) {
+//             counter++;
+//             console.log(letter)
+//         }
+//     }
+// return counter;
+// }
 
 module.exports = vowels;
